4f0e0fd104910c1b9d383910a5699aa3a6ab0306
jhonnymosquera <78888142+jhonnymosquera@users.noreply.github.com>
2022-08-31 11:24:25 -0500

    Update app.js
    Aplicando sugerencias para la optimizacion del codigo.

--- js/app.js
+++ js/app.js
@@ -1,59 +1,42 @@
-// Pregunta
 
-const cardPreguntaUl = document.querySelector('.card__content--ul');
-const list = cardPreguntaUl.children;
-
-const btn1 = list[0].addEventListener('click', btnGris);
-const btn2 = list[1].addEventListener('click', btnGris);
-const btn3 = list[2].addEventListener('click', btnGris);
-const btn4 = list[3].addEventListener('click', btnGris);
-const btn5 = list[4].addEventListener('click', btnGris);
-
-let calificacion = 0;
-
-function btnGris(event){
-
-    for(let i = 0 ; i < list.length; i++){
-        list[i].classList.remove('fondo-gris');
-    };
-
-    const btn = event.target;
-    btn.classList.add('fondo-gris');
-
-    calificacion = parseInt(btn.textContent);
-}
-
-// Respuesta
+const btns = document.querySelectorAll('.card__content--li');
+btns.forEach(btns => btns.addEventListener('click', btnGris));
 
 const submit = document.querySelector('.btn');
 const puntaje = document.querySelector('.puntaje');
 const pregunta = document.querySelector('.pregunta');
 const respuesta = document.querySelector('.respuesta');
 const alert = document.querySelector('.alert');
-
-const pText = document.createElement('P');
-
-function mensaje(text){
-    pText.classList.add('mensaje') ;
-    pText.textContent = text ;
-};
-
 submit.addEventListener('click', btnSubmit);
 
-function btnSubmit(event){
-    event.preventDefault();
-    
-    if(calificacion === 0){
-        
-        mensaje('Please select an answer :)');
-        alert.appendChild(pText);
-        
-        return
-    };
-    
-    mensaje(`You selected ${calificacion} out of ${list.length}`);
-    puntaje.appendChild(pText);
+const pText = document.createElement('P');
+let calificacion = 0;
 
-    pregunta.classList.add('oculto');
-    respuesta.classList.remove('oculto');
-};
+function btnGris(event) {
+	btns.forEach(btn => btn.classList.remove('fondo-gris'));
+
+	const btnEvent = event.target;
+	btnEvent.classList.add('fondo-gris');
+	calificacion = parseInt(btnEvent.textContent);
+}
+
+function mensaje(text) {
+	pText.classList.add('mensaje');
+	pText.textContent = text;
+}
+
+function btnSubmit(event) {
+	event.preventDefault();
+
+	if (calificacion === 0) {
+		mensaje('Please select an answer :)');
+		alert.appendChild(pText);
+		return;
+	}
+
+	mensaje(`You selected ${calificacion} out of ${btns.length}`);
+	puntaje.appendChild(pText);
+
+	pregunta.classList.add('oculto');
+	respuesta.classList.remove('oculto');
+}
